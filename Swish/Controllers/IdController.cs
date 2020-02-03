6dbaea70068e8a0c131333545e8b9de545a87ed8
Alex <Alexpaden1@gmail.com>
2020-02-03 12:27:50 -0800

    Base models for connecting users/profiles/etc Non-functional but backup pre tear down

--- Swish/Controllers/IdController.cs
+++ Swish/Controllers/IdController.cs
@@ -110,4 +110,210 @@ namespace Swish.Controllers
 
 
     }
+    
+    /*
+     // GET: Id/Details/5
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var verificationProfile = await _context.VerificationProfiles
+                    .FirstOrDefaultAsync(m => m.UId == id);
+                if (verificationProfile == null)
+                {
+                    return NotFound();
+                }
+
+                var isAuthorized = await _authorizationService.AuthorizeAsync(User, verificationProfile,
+                    UserOperations.Read);
+
+                if (!isAuthorized.Succeeded)
+                {
+                    return Forbid();
+                }
+
+                return View(verificationProfile);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Id/Details/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(int id, VerificationProfileStatus status)
+        {
+            if (ModelState.IsValid)
+            {
+                var verificationProfile = await _context.VerificationProfiles.FirstOrDefaultAsync(
+                    m => m.UId == id);
+
+                if (verificationProfile == null)
+                {
+                    return NotFound();
+                }
+
+                var userOperation = (status == VerificationProfileStatus.Approved)
+                    ? UserOperations.Approve
+                    : UserOperations.Reject;
+
+                var isAuthorized = await _authorizationService.AuthorizeAsync(User, verificationProfile, userOperation);
+
+                if (!isAuthorized.Succeeded)
+                {
+                    return Forbid();
+                }
+
+                verificationProfile.Status = status;
+                _context.VerificationProfiles.Update(verificationProfile);
+                await _context.SaveChangesAsync();
+
+                return View(verificationProfile);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // GET: Id/Edit/5
+            [HttpGet]
+            public async Task<IActionResult> Edit(int? id)
+            {
+                if (ModelState.IsValid)
+                {
+                    var verificationProfile = await _context.VerificationProfiles
+                        .FirstOrDefaultAsync(m => m.UId == id);
+                    if (verificationProfile == null)
+                    {
+                        return NotFound();
+                    }
+                    var isAuthorized = await _authorizationService.AuthorizeAsync(User, verificationProfile,
+                        UserOperations.Update);
+
+                    if (!isAuthorized.Succeeded)
+                    {
+                        return Forbid();
+                    }
+                    
+                    return View(verificationProfile);
+                }
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            // POST: Id/Edit/5
+            // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+            // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,FakeImgStr, OwnerId")] VerificationProfile verificationProfile)
+            {
+                if (id != verificationProfile.UId)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var isAuthorized = await _authorizationService.AuthorizeAsync(User, verificationProfile,
+                        UserOperations.Update);
+
+                    if (!isAuthorized.Succeeded)
+                    {
+                        return Forbid();
+                    }
+                    try
+                    {
+                        _context.Update(verificationProfile);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!VerificationProfileExists(verificationProfile.UId))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction("Details", "Id", new { id = verificationProfile.UId });
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            // GET: Id/Delete/5
+            [HttpGet]
+            public async Task<IActionResult> Delete(int? id)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                    {
+                        return NotFound();
+                    }
+
+                    var verificationProfile = await _context.VerificationProfiles
+                        .FirstOrDefaultAsync(m => m.UId == id);
+                    if (verificationProfile == null)
+                    {
+                        return NotFound();
+                    }
+
+                    var isAuthorized = await _authorizationService.AuthorizeAsync(
+                        User, verificationProfile,
+                        UserOperations.Delete);
+                    var currentUserId = _userManager.GetUserId(User);
+
+                    if (!isAuthorized.Succeeded)
+                    {
+                        return Forbid();
+                    }
+                    
+                    return View(verificationProfile);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            // POST: Id/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                if (ModelState.IsValid)
+                {
+                    var verificationProfile = await _context.VerificationProfiles.FindAsync(id);
+
+                    if (verificationProfile == null)
+                    {
+                        return NotFound();
+                    }
+
+                    var isAuthorized = await _authorizationService.AuthorizeAsync(User, verificationProfile,
+                        UserOperations.Delete);
+
+                    if (!isAuthorized.Succeeded)
+                    {
+                        return Forbid();
+                    }
+
+                    _context.VerificationProfiles.Remove(verificationProfile);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            private bool VerificationProfileExists(int id)
+            {
+                return _context.VerificationProfiles.Any(e => e.UId == id);
+            }
+        }
+     */
 }
